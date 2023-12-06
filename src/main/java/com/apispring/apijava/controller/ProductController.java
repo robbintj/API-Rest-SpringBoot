233796a0bddc053bcf272cb5909580fe058bc1ad
robbintj <robbintj@hotmail.com>
2023-12-06 01:29:15 +0100

    implementar api

---
+++ src/main/java/com/apispring/apijava/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.apispring.apijava.controller;
+
+import com.apispring.apijava.entities.Department;
+import com.apispring.apijava.entities.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="/products")
+public class ProductController {
+    @GetMapping
+    public List<Product> getObjects(){
+        Department d1 = new Department(1L, "Tech");
+        Department d2 = new Department(2L, "Pet");
+
+        Product p1 = new Product(1L, "Macbook Pro", 4000.0,d1);
+        Product p2 = new Product(2L, "PC Game", 5000.0,d1);
+        Product p3 = new Product(3L, "Pet House", 300.0,d2);
+
+        List<Product> list = Arrays.asList(p1,p2,p3);
+
+        return list;
+
+    }
+
+
+}
